5c2ee70e167896391131db9f1620184b80296a22
Mary Mbugua <marymbugua.nm@gmail.com>
2017-11-30 15:16:04 +0300

    put calculator code on scripts.js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,38 @@
 
  
-      var add = function(number1, number2) {
-        return number1 + number2;
-      };
+    //   var add = function(number1, number2) {
+    //     return number1 + number2;
+    //   };
       
-      //add function
+    //   //add function
       
-    var add = function(number1, number2) {
-          return number1 + number2;
-        };
+    // var add = function(number1, number2) {
+    //       return number1 + number2;
+    //     };
   
-        alert(add(10,5));
+    //     alert(add(10,5));
         
-        //simpe addition only calculator you have to hard code everytime
-      var add = function(number1, number2) {
-          return number1 + number2;
-        };
+    //     //simpe addition only calculator you have to hard code everytime
+    //   var add = function(number1, number2) {
+    //       return number1 + number2;
+    //     };
   
-        var number1 = prompt("Enter a number:");
+    //     var number1 = prompt("Enter a number:");
   
-        alert(number1);
-        //echoes back what the user has inputted
-          var add = function(number1, number2) {
-          return number1 + number2;
-        };
+    //     alert(number1);
+    //     //echoes back what the user has inputted
+    //       var add = function(number1, number2) {
+    //       return number1 + number2;
+    //     };
   
-        var number1 = prompt("Enter a number:");
-        var number2 = prompt("Enter another number:");
+    //     var number1 = prompt("Enter a number:");
+    //     var number2 = prompt("Enter another number:");
   
-        alert(add(number1, number2));
-      //concates the numbers instead of adding
+    //     alert(add(number1, number2));
+    //   //concates the numbers instead of adding
+
+
+
       var add = function(number1, number2) {
           return number1 + number2;
         };
