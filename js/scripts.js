fdbf5280e5958605611e7b1767585e3872b84200
Mary Mbugua <marymbugua.nm@gmail.com>
2017-11-30 15:07:40 +0300

    created calculator project js folder, css folder,sripts.js,index.html and readme.md

---
+++ js/scripts.js
@@ -0,0 +1,42 @@
+
+ 
+      var add = function(number1, number2) {
+        return number1 + number2;
+      };
+      
+      //add function
+      
+    var add = function(number1, number2) {
+          return number1 + number2;
+        };
+  
+        alert(add(10,5));
+        
+        //simpe addition only calculator you have to hard code everytime
+      var add = function(number1, number2) {
+          return number1 + number2;
+        };
+  
+        var number1 = prompt("Enter a number:");
+  
+        alert(number1);
+        //echoes back what the user has inputted
+          var add = function(number1, number2) {
+          return number1 + number2;
+        };
+  
+        var number1 = prompt("Enter a number:");
+        var number2 = prompt("Enter another number:");
+  
+        alert(add(number1, number2));
+      //concates the numbers instead of adding
+      var add = function(number1, number2) {
+          return number1 + number2;
+        };
+  
+        var number1 = parseInt(prompt("Enter a number:"));
+        var number2 = parseInt(prompt("Enter another number:"));
+  
+        alert(add(number1, number2));
+      //working!!full calculator
+         
