a6c14ec6d0d1554a98e47ccffa7401f0e8c90e88
Mary Mbugua <marymbugua.nm@gmail.com>
2017-11-30 15:29:42 +0300

    put subtract,divide and multiply function

--- js/scripts.js
+++ js/scripts.js
@@ -32,7 +32,7 @@
     //   //concates the numbers instead of adding
 
 
-
+/////addition
       var add = function(number1, number2) {
           return number1 + number2;
         };
@@ -41,5 +41,35 @@
         var number2 = parseInt(prompt("Enter another number:"));
   
         alert(add(number1, number2));
-      //working!!full calculator
-         
+      //working!!addition calculator
+
+
+///subtraction
+      var subtract = function(number1, number2) {
+        return number1 - number2;
+      };
+
+      var number1 = parseInt(prompt("Enter a number:"));
+      var number2 = parseInt(prompt("Enter another number:"));
+      var result = subtract(number1, number2);
+      alert(result);
+
+/////multiplication
+      var multiply = function(number1, number2) {
+        return number1 * number2;
+      };
+
+      var number1 = parseInt(prompt("Enter a number:"));
+      var number2 = parseInt(prompt("Enter another number:"));
+      var result = multiply(number1, number2);
+      alert(result);
+
+///division
+        var divide = function(number1, number2) {
+            return number1 / number2;
+        };
+
+        var number1 = parseInt(prompt("Enter a number:"));
+        var number2 = parseInt(prompt("Enter another number:"));
+
+        alert(divide(number1, number2));
